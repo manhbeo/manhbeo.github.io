41b1c00fc52c648dda094ce0a041271885f2d5db
manhbeo <62018096+manhbeo@users.noreply.github.com>
2025-11-28 12:51:04 +0000

    Deploying to gh-pages from @ manhbeo/manhbeo.github.io@dc927bcfc1ae70d577bfceb37a6491e32b281260 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,9 +414,9 @@ ninja.data = [{
           description: "PyTorch implementation of supervised contrastive learning adapted to hierarchical multi-label data.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-cddpm-classifier-free-diffusion-guidance-in-pytorch",
+          title: 'cDDPM: Classifier-Free Diffusion Guidance in PyTorch',
+          description: "Implementation and extensions of classifier-free diffusion guidance, exploring attention and U-Net architectures.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
